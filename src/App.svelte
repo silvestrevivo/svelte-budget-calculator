1245825424046712c1bfab44cad89f2e044e15aa
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-11 11:21:23 +0100

    totals addes using $ reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,13 @@
   // components
   import Navbar from "./Navbar.svelte";
   import ExpensesList from "./ExpensesList.svelte";
+  import Totals from "./Totals.svelte";
   // data
   import expensesData from "./expenses";
   // variables
   let expenses = [...expensesData];
+  // reactive
+  $: total = expenses.reduce((acc, curr) => (acc += curr.amount), 0);
   // functions
   function removeExpense(id) {
     expenses = expenses.filter(item => item.id !== id);
@@ -16,11 +19,13 @@
     expenses = [];
   }
 
+  // Context
   setContext("removeExpense", removeExpense);
 </script>
 
 <Navbar />
 <main class="content">
+  <Totals title="total expenses" {total} />
   <ExpensesList {expenses} />
   <button
     type="button"
---
+++ src/Totals.svelte
@@ -0,0 +1,9 @@
+<script>
+  //Props
+  export let title = "default title";
+  export let total = 0;
+</script>
+
+<section class="main-title">
+  <h2>{title}: ${total}</h2>
+</section>
