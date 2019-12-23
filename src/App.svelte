8a6e38f35f9fa65e58697bb7c4128e67e6a603b8
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-23 13:06:00 +0100

    adding lifecycle methods and localstorage

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,12 @@
 <script>
-  import { setContext } from "svelte";
+  import { setContext, onMount, afterUpdate } from "svelte";
   // components
   import Navbar from "./Navbar.svelte";
   import ExpensesList from "./ExpensesList.svelte";
   import Totals from "./Totals.svelte";
   import ExpenseForm from "./ExpenseForm.svelte";
-  // data
-  import expensesData from "./expenses";
   // variables
-  let expenses = [...expensesData];
+  let expenses = [];
   // set editing variables
   let setName = "";
   let setAmount = null;
@@ -18,6 +16,14 @@
   // reactive
   $: total = expenses.reduce((acc, curr) => (acc += curr.amount), 0);
   $: isEditing = setId ? true : false;
+
+  onMount(() => {
+    expenses = localStorage.getItem("expenses")
+      ? JSON.parse(localStorage.getItem("expenses"))
+      : [];
+  });
+  afterUpdate(() => setLocalStorage());
+
   // functions
   function addExpense({ name, amount }) {
     let expense = {
@@ -70,6 +76,11 @@
   // Context
   setContext("removeExpense", removeExpense);
   setContext("modifyExpense", setModifiedExpense);
+
+  // LocalStorage
+  function setLocalStorage() {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
 </script>
 
 <Navbar {showForm} />
